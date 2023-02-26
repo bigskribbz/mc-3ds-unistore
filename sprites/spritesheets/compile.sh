a31f5eb821f1cf3f8f26b21e48461e58aeaf8727
bigskribbz <126123564+bigskribbz@users.noreply.github.com>
2023-02-26 09:39:43 -0600

    Added chmod command for /opt/devkitpro

--- sprites/spritesheets/compile.sh
+++ sprites/spritesheets/compile.sh
@@ -3,6 +3,7 @@ wget https://apt.devkitpro.org/install-devkitpro-pacman
 chmod +x ./install-devkitpro-pacman
 sudo ./install-devkitpro-pacman
 sudo dkp-pacman -S tex3ds
+sudo chmod -R 777 /opt/devkitpro
 cd /opt/devkitpro/tools/bin/
 ./tex3ds -i /opt/devkitpro/tools/bin/atlas.t3s -o /opt/devkitpro/tools/bin/atlas.t3x
 echo -e "\e[1;32m Textures have been compiled to /opt/devkitpro/tools/bin/atlas.t3x \e[0m"
