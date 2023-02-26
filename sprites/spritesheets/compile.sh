c1d9ce0a8ab149671b4338dd8240d647d19320c4
bigskribbz <126123564+bigskribbz@users.noreply.github.com>
2023-02-26 09:45:34 -0600

    Update compile.sh

--- sprites/spritesheets/compile.sh
+++ sprites/spritesheets/compile.sh
@@ -5,5 +5,5 @@ sudo ./install-devkitpro-pacman
 sudo dkp-pacman -S tex3ds
 sudo chmod -R 777 /opt/devkitpro
 cd /opt/devkitpro/tools/bin/
-./tex3ds -i /opt/devkitpro/tools/bin/atlas.t3s -o /opt/devkitpro/tools/bin/atlas.t3x
-echo -e "\e[1;32m Textures have been compiled to /opt/devkitpro/tools/bin/atlas.t3x \e[0m"
+./tex3ds -i /opt/devkitpro/tools/bin/sprites/atlas.t3s -o ~/atlas.t3x
+echo -e "\e[1;32m Textures have been compiled to atlas.t3x in the Home folder. \e[0m"
